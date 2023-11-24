1f6e4b8567c00c3b00fda42aa5f5236763400676
Waleed Murad <126694427+waleedmurad91@users.noreply.github.com>
2023-11-24 14:38:53 +0500

    quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -1,27 +1,13 @@
+# Import necessary libraries
 import streamlit as st
-import nltk
 from nltk import ngrams
 from nltk.tokenize import word_tokenize
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-# Download NLTK data
-nltk.download('punkt')
 
 # Function to generate n-grams
 def generate_ngrams(text, n):
     tokens = word_tokenize(text)
     n_grams = list(ngrams(tokens, n))
     return n_grams
-
-# Function to generate and display word cloud
-def display_word_cloud(text):
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.pyplot()
 
 # Streamlit web application
 def main():
@@ -39,10 +25,6 @@ def main():
         n_grams = generate_ngrams(text_input, n)
         st.write(f"{n}-grams:")
         st.write(n_grams)
-
-        # Display word cloud
-        text_for_wordcloud = ' '.join([' '.join(gram) for gram in n_grams])
-        display_word_cloud(text_for_wordcloud)
 
 if __name__ == "__main__":
     main()
